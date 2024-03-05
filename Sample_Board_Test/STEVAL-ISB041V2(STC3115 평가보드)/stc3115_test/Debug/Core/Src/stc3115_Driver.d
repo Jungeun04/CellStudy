958d8605ea7a43b33f549c5d8d52e0a0e7093ef2
Me <h>
2024-03-05 16:40:47 +0900

    add testCode

---
+++ Sample_Board_Test/STEVAL-ISB041V2(STC3115 평가보드)/stc3115_test/Debug/Core/Src/stc3115_Driver.d
@@ -0,0 +1,6 @@
+Core/Src/stc3115_Driver.o: ../Core/Src/stc3115_Driver.c \
+ ../Core/Inc/stc3115_Driver.h ../Core/Inc/stc3115_Battery.h \
+ ../Core/Inc/stc3115_I2C.h
+../Core/Inc/stc3115_Driver.h:
+../Core/Inc/stc3115_Battery.h:
+../Core/Inc/stc3115_I2C.h:
